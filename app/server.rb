3bc8fe287707c9a2ef13041781f0d9e895670965
Tsuyoshi Ushio <ushio@simplearchitect.com>
2017-07-11 00:20:47 +0900

    Initial commit

---
+++ app/server.rb
@@ -0,0 +1,82 @@
+#!/usr/bin/ruby
+#
+# Copyright 2017 Istio Authors
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+require 'webrick'
+
+if ARGV.length < 1 then
+    puts "usage: #{$PROGRAM_NAME} port"
+    exit(-1)
+end
+
+port = Integer(ARGV[0])
+
+server = WEBrick::HTTPServer.new :BindAddress => '0.0.0.0', :Port => port
+
+trap 'INT' do server.shutdown end
+
+index = '
+<html>
+<head>
+    <title>Blue Version 1.0.0</title>
+</head>
+<body bgcolor="#0000FF">
+    <H1>This is Blue Version 1.0.0</H1>
+</body>
+</html>
+'
+
+login = '
+<html>
+<head>
+    <title>V2Tester Login</title>
+    <meta http-equiv="Set-Cookie" content="NAME=v2tester"> 
+</head>
+<body>
+    <H1>You logged in as a v2 Tester.</H1>
+</body>
+</html>
+'
+
+logout = '
+<html>
+<head>
+    <title>V2Tester Logout</title>
+    <meta http-equiv="Set-Cookie" content="NAME=v2tester; expires=Fri, 31-Dec-1999 23:59:59 GMT;"> 
+</head>
+<body>
+    <H1>You logged out as a v2 Tester.</H1>
+</body>
+</html>
+
+'
+
+server.mount_proc '/webpage' do |req, res|
+    res.status = 200
+    res.body = index
+    res['Content-Type'] = 'text/html'
+end
+
+server.mount_proc '/loginpage' do |req, res|
+    res.body = login
+    res['Content-Type'] = 'text/html'
+end
+
+server.mount_proc '/logoutpage' do |req, res|
+    res.body = logout
+    res['Content-Type'] = 'text/html'
+end
+
+server.start
---
+++ bluegreen/memo.md
@@ -0,0 +1,41 @@
+HTTP/1.1 200 OK
+content-type: text/html; charset=utf-8
+content-length: 4744
+server: envoy
+date: Mon, 10 Jul 2017 15:10:49 GMT
+x-envoy-upstream-service-time: 257
+
+GET /productpage HTTP/1.1
+Host: 104.40.59.251
+Connection: keep-alive
+Pragma: no-cache
+Cache-Control: no-cache
+Upgrade-Insecure-Requests: 1
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8
+Accept-Encoding: gzip, deflate
+Accept-Language: en-US,en;q=0.8
+Cookie: name=ushio; user=jason
+
+---- index.html
+HTTP/1.1 200 OK
+server: envoy
+date: Mon, 10 Jul 2017 15:13:08 GMT
+content-type: text/html
+content-length: 141
+last-modified: Sun, 09 Jul 2017 13:37:59 GMT
+etag: "596231b7-8d"
+accept-ranges: bytes
+x-envoy-upstream-service-time: 8
+
+GET /web/index.html HTTP/1.1
+Host: 104.40.59.251
+Connection: keep-alive
+Pragma: no-cache
+Cache-Control: no-cache
+Upgrade-Insecure-Requests: 1
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8
+Accept-Encoding: gzip, deflate
+Accept-Language: en-US,en;q=0.8
+Cookie: name=ushio; user=jason
